8d2cedd769d07ec4d262fdcfb7648c396e00bafb
ucmucm <007smiker@gmail.com>
2017-09-16 01:56:46 -0400

    Update HH:MM.java

--- Google/HH:MM.java
+++ Google/HH:MM.java
@@ -11,44 +11,48 @@ class Solution {
             set.add(time.charAt(i)-48);
         }
         int point = 0;
-        for (int i=4; i>=0; i--){
-            if (i == 2) {
-                res.append(':');
-                continue;
-            }
-            if (i == 4){
-                if (set.higher(time.charAt(i)-48) == null) point = 1;
-                if (point == 1) res.append(set.first());
-                else res.append(set.higher(time.charAt(i)-48));
-            }
-            else if (i == 3){
-                if (point == 0) res.append(time.charAt(i));
-                else{
-                    if (set.higher(time.charAt(i) - 48) == null || set.higher(time.charAt(i)-48) > 5) res.append(set.first());
-                    else {
-                        res.append(set.higher(time.charAt(i)-48));
-                        point = 0;
-                    }
-                }
-            }
-            else if (i == 1){
-                if (point == 0) res.append(time.charAt(i));
-                else {
-                    if (set.higher(time.charAt(i) - 48) == null || set.higher(time.charAt(i)-48) > 3) res.append(set.first());
-                    else {
-                        res.append(set.higher(time.charAt(i) - 48));
-                        point = 0;
-                    }
-                }
-            }
+        // 最后一位
+        if (set.higher(time.charAt(4)-48) == null) point = 1;
+        if (point == 1) res.append(set.first());
+        else res.append(set.higher(time.charAt(4)-48));
+
+        // 倒数第二
+        if (point == 0) res.append(time.charAt(3));
+        else {
+            if (set.higher(time.charAt(3) - 48) == null || set.higher(time.charAt(3) - 48) > 5)
+                res.append(set.first());
             else {
-                if (point == 0) res.append(time.charAt(i));
-                else{
-                    if (set.higher(time.charAt(i)-48) == null || set.higher(time.charAt(i)-48) > 2) res.append(set.first());
-                    else res.append(set.higher(time.charAt(i)-48));
+                res.append(set.higher(time.charAt(3) - 48));
+                point = 0;
+            }
+        }
+        res.append(':');
+        if (point == 0) res.append(time.charAt(1));
+        else {
+            if (time.charAt(0) == 2) {
+                if (set.higher(time.charAt(1) - 48) == null || set.higher(time.charAt(1) - 48) > 3)
+                    res.append(set.first());
+                else {
+                    res.append(set.higher(time.charAt(1) - 48));
+                    point = 0;
+                }
+            }
+            else{
+                if (set.higher(time.charAt(1) - 48) == null)
+                    res.append(set.first());
+                else {
+                    res.append(set.higher(time.charAt(1) - 48));
+                    point = 0;
                 }
             }
         }
+
+        if (point == 0) res.append(time.charAt(0));
+        else{
+            if (set.higher(time.charAt(0)-48) == null || set.higher(time.charAt(0)-48) > 2) res.append(set.first());
+            else res.append(set.higher(time.charAt(0)-48));
+        }
+
         res.reverse();
         return res.toString();
     }
